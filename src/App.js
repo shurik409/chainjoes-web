18ee2e28921232e1f9b70f5f01b1013f4315d1f5
AlexKaraychentsev <shurik409@gmial.com>
2022-10-01 00:42:49 +0300

    revert explore

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Box } from "@mui/material";
-import { Main, PrivacyPolicy, Terms, Test } from "./pages";
+import { Main, PrivacyPolicy, Terms } from "./pages";
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="privacy-policy" element={<PrivacyPolicy />} />
         <Route path="terms-conditions" element={<Terms />} />
-        <Route path="test" element={<Test />} />
       </Routes>
     </Box>
   );
